0cf3bfbfe8c11bc04832e0175ed38e97fe3174cb
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-07-07 11:37:44 -0400

    test

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/Masterminds/squirrel v1.5.0 // indirect
+	github.com/jackc/pgtype v1.7.0 // indirect
 	github.com/jackc/pgx/v4 v4.11.0 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 )
--- tests/helper_test.go
+++ tests/helper_test.go
@@ -1,1 +1,27 @@
 package pgkit_test
+
+import (
+	"time"
+
+	"github.com/jackc/pgtype"
+)
+
+type Account struct {
+	ID        int64     `db:"id,omitempty"`
+	Name      string    `db:"name"`
+	Disabled  bool      `db:"disabled"`
+	CreatedAt time.Time `db:"created_at,omitempty"`
+}
+
+type Review struct {
+	ID        int64     `db:"id,omitempty"`
+	Name      string    `db:"name"`
+	Comments  string    `db:"comments"`
+	CreatedAt time.Time `db:"created_at"`
+}
+
+type Log struct {
+	ID      int64        `db:"id,omitempty"`
+	Message string       `db:"message"`
+	Etc     pgtype.JSONB `db:"etc"`
+}
