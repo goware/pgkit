6377c57349bcc54348839c6267b375f77b01e418
Marcin Gorzynski <marcin@gorzynski.me>
2025-03-04 12:01:02 +0100

    tx query from context

--- utils.go
+++ utils.go
@@ -1,6 +1,10 @@
 package pgkit
 
-import "github.com/jackc/pgx/v5"
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5"
+)
 
 var ErrNoRows = pgx.ErrNoRows
 
@@ -9,3 +13,18 @@ type errRow struct {
 }
 
 func (e errRow) Scan(dest ...interface{}) error { return e.err }
+
+var txContextKey = &struct{}{}
+
+func WithTx(ctx context.Context, tx pgx.Tx) context.Context {
+	return context.WithValue(ctx, txContextKey, tx)
+}
+
+func NoTx(ctx context.Context) context.Context {
+	return WithTx(ctx, nil)
+}
+
+func TxFromContext(ctx context.Context) pgx.Tx {
+	tx, _ := ctx.Value(txContextKey).(pgx.Tx)
+	return tx
+}
