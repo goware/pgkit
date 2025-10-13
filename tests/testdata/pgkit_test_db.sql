3569547e1cf874acc244edd6c9ae3581cd9e8472
Vojtech Vitek <vojtech.vitek@golang.cz>
2025-10-13 22:18:10 +0200

    Add tests for simple CRUD, complex transactions and LockForUpdate()

--- tests/testdata/pgkit_test_db.sql
+++ tests/testdata/pgkit_test_db.sql
@@ -6,12 +6,27 @@ CREATE TABLE accounts (
   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE articles (
+  id SERIAL PRIMARY KEY,
+  author VARCHAR(80) NOT NULL,
+  alias VARCHAR(80),
+  content JSONB,
+  account_id INTEGER NOT NULL REFERENCES accounts(id),
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted_at TIMESTAMP WITHOUT TIME ZONE NULL
+);
+
 CREATE TABLE reviews (
   id SERIAL PRIMARY KEY,
-  -- article_id integer,
-  name VARCHAR(80),
-  comments TEXT,
-  created_at TIMESTAMP WITHOUT TIME ZONE
+  article_id INTEGER REFERENCES articles(id),
+  account_id INTEGER NOT NULL REFERENCES accounts(id),
+  comment TEXT,
+  status SMALLINT,
+  sentiment SMALLINT,
+  created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  deleted_at TIMESTAMP WITHOUT TIME ZONE NULL
 );
 
 CREATE TABLE logs (
@@ -26,11 +41,4 @@ CREATE TABLE stats (
   key VARCHAR(80) UNIQUE,
   big_num NUMERIC(78,0) NOT NULL, -- representing a big.Int runtime type
   rating NUMERIC(78,0) NULL -- representing a nullable big.Int runtime type
-);
-
-CREATE TABLE articles (
-  id SERIAL PRIMARY KEY,
-  author VARCHAR(80) NOT NULL,
-  alias VARCHAR(80),
-  content JSONB
 );
