39c8250d4a1e9161c4b3675e9f4a51c82293ad4c
Vojtech Vitek <vojtech.vitek@golang.cz>
2025-10-21 13:20:37 +0200

    Fix updated_at field, thanks @shunkakinoki

--- tests/testdata/pgkit_test_db.sql
+++ tests/testdata/pgkit_test_db.sql
@@ -3,7 +3,8 @@ CREATE TABLE accounts (
   name VARCHAR(255),
   disabled BOOLEAN,
   new_column_not_in_code BOOLEAN, -- test for backward-compatible migrations, see https://github.com/goware/pgkit/issues/13
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
 CREATE TABLE articles (
